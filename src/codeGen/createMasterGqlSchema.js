fcc92a979ea9959b0489b6fcdc75504aa7ea0abb
Duncan Healy <duncan.healy@gmail.com>
2020-05-15 13:29:46 +0100

    add mutation extras to tableless, use module alias based on project root

--- src/codeGen/createMasterGqlSchema.js
+++ src/codeGen/createMasterGqlSchema.js
@@ -3,6 +3,15 @@ import _globalSchemaTypes from "./globalSchemaTypes";
 const globalSchemaTypes = _globalSchemaTypes.replace(/^  /gm, "");
 
 import createTypeSchemaHelpers from "./typeSchemaHelpers";
+// try {
+//   require("module-alias/register")
+
+//   require("module-alias").addAliases({
+//     "@": __dirname
+//   })
+// } catch (error) {
+//   console.error(error)
+// }
 
 export default function createMasterGqlSchema(types, rootDir) {
   const typesWithTables = types.filter(t => t.table);
@@ -10,24 +19,19 @@ export default function createMasterGqlSchema(types, rootDir) {
 
   const tablelessTypePackets = typesWithoutTables.map(objectToCreate => {
     let packet = createTypeSchemaHelpers(objectToCreate);
-
-    return {
-      types: packet.createSchemaTypes()
-    };
-  });
-
-  const tableTypePackets = typesWithTables.map(objectToCreate => {
-    let packet = createTypeSchemaHelpers(objectToCreate);
-    let objName = objectToCreate.__name;
-    let modulePath = path.join(rootDir, objName);
-
     let extras = objectToCreate.extras || {};
     let schemaSources = extras.schemaSources || [];
     let extraQueries = [];
     let extraMutations = [];
-
+    const findPath = path.join(rootDir, "../..");
     schemaSources.forEach(src => {
-      let newPath = path.join(modulePath, src);
+      // check for module alias
+      // const findPath = modulePath //
+      // if(src.indexOf("@")===0){
+      //   src =src.replace(src,"./")
+      // }
+
+      let newPath = path.join(findPath, src.replace("@", ""));
       let ext = path.extname(src);
       if (!ext) {
         newPath += ".js";
@@ -39,10 +43,46 @@ export default function createMasterGqlSchema(types, rootDir) {
     });
 
     return {
+      types: packet.createSchemaTypes(),
+      query: extraQueries.length ? "\n" + extraQueries.join("\n") : "",
+      mutation: extraMutations.length ? "\n" + extraMutations.join("\n") : "",
+    };
+  });
+
+  const tableTypePackets = typesWithTables.map(objectToCreate => {
+    let packet = createTypeSchemaHelpers(objectToCreate);
+    let objName = objectToCreate.__name;
+    let modulePath = path.join(rootDir, objName);
+    const findPath = path.join(rootDir, "../..");
+    let extras = objectToCreate.extras || {};
+    let schemaSources = extras.schemaSources || [];
+    let extraQueries = [];
+    let extraMutations = [];
+
+    schemaSources.forEach(src => {
+      // check for module alias
+      // const findPath = modulePath //
+      // if(src.indexOf("@")===0){
+      //   src =src.replace(src,"./")
+      // }
+
+      let newPath = path.join(findPath, src.replace("@", ""));
+      let ext = path.extname(src);
+      if (!ext) {
+        newPath += ".js";
+      }
+
+      let esModule = require(newPath).default;
+      esModule.Query && extraQueries.push(esModule.Query);
+      esModule.Mutation && extraMutations.push(esModule.Mutation);
+      console.table(extraMutations);
+    });
+
+    return {
       readonly: objectToCreate.readonly,
       types: packet.createSchemaTypes(),
       query: packet.createQueryType() + (extraQueries.length ? "\n" + extraQueries.join("\n") : ""),
-      mutation: packet.createMutationType() + (extraMutations.length ? "\n" + extraMutations.join("\n") : "")
+      mutation: packet.createMutationType() + (extraMutations.length ? "\n" + extraMutations.join("\n") : ""),
     };
   });
 
@@ -58,15 +98,18 @@ ${tableTypePackets.map(p => p.query).join("\n\n")}
 }
 
 ${
-  tableTypePackets.find(t => !t.readonly)
+  tableTypePackets.find(t => !t.readonly) || tablelessTypePackets.find(t => !t.readonly)
     ? `type Mutation {
 ${tableTypePackets
   .filter(t => !t.readonly)
   .map(p => p.mutation)
   .join("\n\n")}
+  ${tablelessTypePackets
+    .filter(t => !t.readonly)
+    .map(p => p.mutation)
+    .join("\n\n")}
 }`
     : ""
 }
-
 `.trim();
 }
